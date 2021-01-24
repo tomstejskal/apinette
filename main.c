c851423ae61474da433e0dbd1a6a9a627fef8b9e
Tom Stejskal <tom.stejskal@gmail.com>
2021-01-24 17:20:54 +0100

    basic implementation of REPL

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <lauxlib.h>
 #include <lua.h>
 #include <lualib.h>
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,31 +15,48 @@
 #include "utlist.h"
 #include "utstring.h"
 
+#define PROGNAME "apinette"
+
 int main(int argc, char **argv) {
   char *err = NULL;
   int res = EXIT_SUCCESS;
   char *script;
   lua_State *L = NULL;
   int lua_err;
+  char *buf;
 
-  if (argc != 2) {
+  if (argc > 2) {
     fprintf(stderr, "Usage: %s SCRIPT\n", argv[0]);
     res = EXIT_FAILURE;
     goto end;
   }
-  script = argv[1];
 
   api_init(&err);
-
   L = luaL_newstate();
   luaL_openlibs(L);
   api_init_lua(L);
-  lua_err = luaL_loadfile(L, script) || lua_pcall(L, 0, 0, 0);
-  if (lua_err) {
-    fprintf(stderr, "%s\n", lua_tostring(L, -1));
-    lua_pop(L, 1);
-    res = EXIT_FAILURE;
-    goto end;
+
+  if (argc == 1) {
+    while ((buf = readline(PROGNAME "> "))) {
+      lua_err = luaL_loadbuffer(L, buf, strlen(buf), "apinette") ||
+                lua_pcall(L, 0, 0, 0);
+      if (lua_err) {
+        fprintf(stderr, "%s\n", lua_tostring(L, -1));
+        lua_pop(L, 1);
+      } else {
+        add_history(buf);
+      }
+      free(buf);
+    }
+  } else {
+    script = argv[1];
+    lua_err = luaL_loadfile(L, script) || lua_pcall(L, 0, 0, 0);
+    if (lua_err) {
+      fprintf(stderr, "%s\n", lua_tostring(L, -1));
+      lua_pop(L, 1);
+      res = EXIT_FAILURE;
+      goto end;
+    }
   }
 
 end:
