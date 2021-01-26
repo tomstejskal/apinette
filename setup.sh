23078a88d4ca653e17c64e62c96bf98acf334c68
Tom Stejskal <tom.stejskal@gmail.com>
2021-01-26 13:18:33 +0100

    allow select compiler

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ print_usage() {
   echo "        -h|--help               print this help"
   echo "        -d|--debug              setup debug build"
   echo "        -b|--build PATH         build path (defult 'build')"
+  echo "        -c|--compiler NAME      compiler binary (gcc, clang)"
   echo
 }
 
 OPTIONS=""
 BUILD="build"
+export CC
 
 while [ ! -z $1 ]; do
   case "$1" in
@@ -27,6 +29,10 @@ while [ ! -z $1 ]; do
       BUILD="$2"     
       shift 2
     ;;
+    '-c' | '--compiler')
+      CC="$2"
+      shift 2
+    ;;
     *)
       echo "Unknown option '$1'" >&2
       exit 1
@@ -35,5 +41,4 @@ while [ ! -z $1 ]; do
 done
 
   
-#meson setup --buildtype=debug build -Db_sanitize=address,undefined
 meson setup $OPTIONS $BUILD
