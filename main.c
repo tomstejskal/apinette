9cc503cc091352391b51e7f47e521cf93e3e1c79
Tom Stejskal <tom.stejskal@gmail.com>
2021-03-12 22:12:20 +0100

    cleanup the main function

--- main.c
+++ main.c
@@ -17,87 +17,70 @@
 #define PROGNAME "apinette"
 #define HISTORY_FILE ".apinette_history"
 
-int main(int argc, char **argv) {
-  char *err = NULL;
-  int res = EXIT_SUCCESS;
-  char *script;
-  lua_State *L = NULL;
-  int lua_err;
+int repl(lua_State *L) {
   char *line;
-  char *brk;
-  char *tok;
+  int lua_err;
+
+  linenoiseSetMultiLine(1);
+  linenoiseHistoryLoad(HISTORY_FILE);
+  while ((line = linenoise(PROGNAME "> "))) {
+    lua_err = luaL_loadstring(L, line) || lua_pcall(L, 0, 0, 0);
+    if (lua_err) {
+      fprintf(stderr, "%s\n", lua_tostring(L, -1));
+      lua_pop(L, 1);
+    }
+    linenoiseHistoryAdd(line);
+    linenoiseHistorySave(HISTORY_FILE);
+    free(line);
+  }
+  return EXIT_SUCCESS;
+}
+
+int run_script(lua_State *L, const char *script) {
   char *tmp;
   char *dir;
   const char *path;
-  int len;
+  int lua_err;
+
+  tmp = malloc(strlen(script) + 1);
+  strcpy(tmp, script);
+  dir = dirname(tmp);
+  if (dir && strlen(dir)) {
+    lua_getglobal(L, "package");
+    lua_getfield(L, -1, "path");
+    path = lua_tostring(L, -1);
+    lua_pop(L, 1);
+    lua_pushfstring(L, "%s;%s/?.lua", path, dir);
+    lua_setfield(L, -2, "path");
+  }
+  free(tmp);
+  lua_err = luaL_loadfile(L, script) || lua_pcall(L, 0, 0, 0);
+  if (lua_err) {
+    fprintf(stderr, "%s\n", lua_tostring(L, -1));
+    lua_pop(L, 1);
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+  char *err = NULL;
+  int res = EXIT_SUCCESS;
+  lua_State *L = NULL;
 
   if (argc > 2) {
     fprintf(stderr, "Usage: %s SCRIPT\n", argv[0]);
-    res = EXIT_FAILURE;
-    goto end;
+    return EXIT_FAILURE;
   }
 
   L = api_init(&err);
 
   if (argc == 1) {
-    linenoiseSetMultiLine(1);
-    linenoiseHistoryLoad(HISTORY_FILE);
-    while ((line = linenoise(PROGNAME "> "))) {
-      tmp = NULL;
-      brk = strpbrk(line, " \t\r\n({");
-      if (brk) {
-        len = brk - line;
-      } else {
-        len = strlen(line);
-      }
-      tok = malloc(len + 1);
-      strncpy(tok, line, len);
-      tok[len] = 0;
-      if (!((strcmp(tok, "print") == 0) || (strcmp(tok, "if") == 0) ||
-            (strcmp(tok, "for") == 0) || (strcmp(tok, "while") == 0) ||
-            (strcmp(tok, "function") == 0))) {
-        tmp = api_printf("print(%s)", line);
-      }
-      free(tok);
-      if (!tmp) {
-        tmp = line;
-      }
-      lua_err = luaL_loadstring(L, tmp) || lua_pcall(L, 0, 0, 0);
-      if (lua_err) {
-        fprintf(stderr, "%s\n", lua_tostring(L, -1));
-        lua_pop(L, 1);
-      }
-      linenoiseHistoryAdd(line);
-      linenoiseHistorySave(HISTORY_FILE);
-      if (tmp != line) {
-        free(tmp);
-      }
-      free(line);
-    }
+    res = repl(L);
   } else {
-    script = argv[1];
-    tmp = malloc(strlen(script) + 1);
-    strcpy(tmp, script);
-    dir = dirname(tmp);
-    if (dir && strlen(dir)) {
-      lua_getglobal(L, "package");
-      lua_getfield(L, -1, "path");
-      path = lua_tostring(L, -1);
-      lua_pop(L, 1);
-      lua_pushfstring(L, "%s;%s/?.lua", path, dir);
-      lua_setfield(L, -2, "path");
-    }
-    free(tmp);
-    lua_err = luaL_loadfile(L, script) || lua_pcall(L, 0, 0, 0);
-    if (lua_err) {
-      fprintf(stderr, "%s\n", lua_tostring(L, -1));
-      lua_pop(L, 1);
-      res = EXIT_FAILURE;
-      goto end;
-    }
+    res = run_script(L, argv[1]);
   }
 
-end:
   api_cleanup(L);
   return res;
 }
